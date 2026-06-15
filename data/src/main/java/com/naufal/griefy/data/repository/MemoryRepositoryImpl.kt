66938695b6817554569eb1770314e910a58a82a4
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2026-06-15 23:46:29 +0800

    Add MemoryRepositoryImpl

---
+++ data/src/main/java/com/naufal/griefy/data/repository/MemoryRepositoryImpl.kt
@@ -0,0 +1,58 @@
+package com.naufal.griefy.data.repository
+
+import com.naufal.griefy.data.local.MemoryDao
+import com.naufal.griefy.domain.model.Memory
+import com.naufal.griefy.domain.repository.MemoryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+
+class MemoryRepositoryImpl @Inject constructor(
+    private val dao: MemoryDao
+) : MemoryRepository {
+
+    override fun getAllMemories(): Flow<List<Memory>> {
+
+        return dao.getAllMemories().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override fun getPublicMemories(): Flow<List<Memory>> {
+
+        return dao.getAllMemories().map { entities ->
+            entities.filter { it.isPublic }.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun getMemoryById(id: Int): Memory? {
+        return dao.getMemoryById(id)?.toDomain()
+    }
+
+    override suspend fun addMemory(memory: Memory) {
+        dao.insertMemory(memory.toEntity())
+    }
+
+    override suspend fun updateMemory(memory: Memory) {
+        dao.updateMemory(memory.toEntity())
+    }
+
+    override suspend fun moveToTrash(id: Int) {
+        dao.moveToTrash(id)
+    }
+
+    override fun getTrashedMemories(): Flow<List<Memory>> {
+        return dao.getTrashedMemories().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun restoreFromTrash(id: Int) {
+        dao.restoreFromTrash(id)
+    }
+
+    override suspend fun deletePermanently(id: Int) {
+        dao.deletePermanently(id)
+    }
+}
