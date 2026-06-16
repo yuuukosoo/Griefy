ff30d5b61b5b6f99a09c7565bf7d2fe4624044e1
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2026-06-16 08:54:27 +0800

    add navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 }
 
 dependencies {
+
+
     implementation(project(":domain"))
     implementation(project(":data"))
 
@@ -73,4 +75,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
