c74dfede713f571e4435356eb055104f54434da8
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2026-06-17 19:21:31 +0800

    fix: Reduce Spotify search limit to 10 to resolve Invalid Limit error

--- data/src/main/java/com/naufal/griefy/data/remote/SpotifyApi.kt
+++ data/src/main/java/com/naufal/griefy/data/remote/SpotifyApi.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface SpotifyApi {
 
 
-    @GET("v1/search?type=track&limit=15")
+    @GET("v1/search?type=track&limit=10")
     suspend fun searchTracks(
         @Header("Authorization") token: String,
         @Query("q") query: String,
