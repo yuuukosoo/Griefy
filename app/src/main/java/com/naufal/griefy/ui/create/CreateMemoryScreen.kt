f3095b550bef6d497fef73bb6844f7e60f8850d6
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2026-06-17 09:57:06 +0800

    Work Manager background Task Trash

--- app/src/main/java/com/naufal/griefy/ui/create/CreateMemoryScreen.kt
+++ app/src/main/java/com/naufal/griefy/ui/create/CreateMemoryScreen.kt
@@ -109,7 +109,7 @@ fun CreateMemoryScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            
+
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
